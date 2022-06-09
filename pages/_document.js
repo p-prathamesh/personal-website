217aa5f1404f443642d3afa8ca7863027fe229ee
p-prathamesh <p-prathamesh@users.noreply.github.com>
2022-06-09 09:20:23 +0000

    Prettified Code!

--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,7 @@ export default function MyDocument() {
         `,
           }}
         />
-         <script
+        <script
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS}`}
         />
